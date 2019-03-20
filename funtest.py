007388451b587aa98a393a99c81c5f40944e1142
lao <asdga.123@163.com>
2019-03-20 08:42:56 -0700

    Front page HTML now generated from a template

--- funtest.py
+++ funtest.py
@@ -24,16 +24,18 @@ class NewVistirorTest(unittest.TestCase):
             'Enter a to-do item')
 
         inputbox.send_keys('Buy peacock feathers')
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
+
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table" )
+
         self.fail('Finish the test!')
+
+
 if __name__=='__main__':
     unittest.main(warnings='ignore')
 
