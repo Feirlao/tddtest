066ecf82df258f014f81a7f2cb3d7afd786ca135
lao <asdga.123@163.com>
2019-03-25 09:24:41 -0700

    page 214

--- superlists/settings.py
+++ superlists/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'django',
+         'USER':  'root',
+         'PASSWORD':'root',
+         #'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        #}
+      }
 }
 
 
